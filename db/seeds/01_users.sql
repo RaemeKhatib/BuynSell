bb1a67d3fedb8f8af68047570d954080f3da1e15
Ebele <ebele.omanukwue@telus.com>
2022-12-19 17:16:42 -0700

    update the database to make changes to table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,11 +3,11 @@ INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@wonderland.ca'
 INSERT INTO users (name, email, password) VALUES ('Kira','kira@myworld.ca', '12345');
 
 
-INSERT INTO products (owner_id, name, description, image_url, status)
+INSERT INTO products (user_id, name, description, image_url, status)
 VALUES (1, 'Xperia Pureness', 'Who doesnt want a see-through screen and the ability to see into thr future?', 'https://www.boredpanda.com/blog/wp-content/uploads/2022/11/1592485323875192832-png__700-6374a223ae332__700.jpg', true),
 
 (2, 'Nokia Sneaker', 'This phone has the ability to turn into a camera that allows you to see around corners, perfect when looking to avoid your ex', 'https://www.boredpanda.com/blog/wp-content/uploads/2022/11/1592140102318587904-png__700.jpg', true),
 
 
-(3, 'Prison Break', 'This super sleak design is made to help you get ur phone into all sorts of places, including ones where its frowned upon', 'https://www.boredpanda.com/blog/wp-content/uploads/2022/11/1592254518003388418-png__700.jpg', true)
+(1, 'Prison Break', 'This super sleak design is made to help you get ur phone into all sorts of places, including ones where its frowned upon', 'https://www.boredpanda.com/blog/wp-content/uploads/2022/11/1592254518003388418-png__700.jpg', true)
 
