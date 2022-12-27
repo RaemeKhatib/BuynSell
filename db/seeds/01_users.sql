fcf1824f6fd79fb84c6ebef799c3528b6166dcbc
Ebele <ebele.omanukwue@telus.com>
2022-12-27 12:29:48 -0700

    implement function to load product on Buynsell app homepage

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,10 +4,10 @@ INSERT INTO users (name, email, password) VALUES ('Kira','kira@myworld.ca', '123
 
 
 INSERT INTO products (user_id, name, description, image_url, status)
-VALUES (1, 'Xperia Pureness', 'Who doesnt want a see-through screen and the ability to see into thr future?', 'https://preview.redd.it/t2ra7xu0yea61.jpg?width=3024&format=pjpg&auto=webp&s=2f1afea73b52901385b92c1195abfe1e10d1ddb0', true),
+VALUES (1, 'Xperia Pureness', 'Who doesnt want a see-through screen and the ability to see into the future? This device will blow your mind.', 'https://preview.redd.it/t2ra7xu0yea61.jpg?width=3024&format=pjpg&auto=webp&s=2f1afea73b52901385b92c1195abfe1e10d1ddb0', true),
 
 (2, 'Nokia Sneaker', 'This phone has the ability to turn into a camera that allows you to see around corners, perfect when looking to avoid your ex', 'https://scontent.fyxd2-1.fna.fbcdn.net/v/t1.18169-9/10401239_89903102396_6483758_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=Vnfm_iMME00AX-SmH3t&_nc_ht=scontent.fyxd2-1.fna&oh=00_AfD_Cb9GdEgnYJeq0Y-ZYBeMejyeLfLi4MOepCd_Sj5j_g&oe=63CACBE6', true),
 
 
-(1, 'Prison Break', 'This super sleak design is made to help you get ur phone into all sorts of places, including ones where its frowned upon', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGHaecyUN3NcM7j2NKfIQiGaDXLxbuXkeMWvP8P3nQ5HadK2HL', true)
+(1, 'Prison Break', 'This super sleak design is made to help you get ur phone into all sorts of places, including ones where its frowned upon.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGHaecyUN3NcM7j2NKfIQiGaDXLxbuXkeMWvP8P3nQ5HadK2HL', true)
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,70 @@
 // // Client facing scripts here
-// $(document).ready(function() {
-
-
-// const createProduct = function(data){
-
-// const $products = $(`<article class="Products">
-// <header>
-
-
-// <div class="Product_image"><${data.products.image_url}>
-
-// </div>
-
-// </header>
-// <section class="Product_info">
-// <div class="Product_name"><b>${data.products.name}</b></div>
-// <div class="description">${data.products.description}</div>
-// <div class="status">Status: <b>Available</b></div>
-// <div class="cart_link">
-// <a href="default.asp"><i class="fa-solid fa-heart"></i></a>
-// <div class="add_Cart">
-// <button type="submit" class="cart-button">Add To Cart</button>
-// </div>
-// </section>
-// <footer>
-// </div>
-// </footer>
-// </header>
-// </article>`);
-
-// return $products;
-
-
-// };
-
-// const renderProducts =(data) => {
-// $(".product-container").empty();
-// //loop throuh the array
-// for(const index of data){
-//   const $products = createProduct(index)
-//   $(".product-container").prepend($products);
-// }
-// };
-
-// const loadProducts = () => {
-//   $.ajax('/products', { method: 'GET' })
-//     .then((product) => {
-//       renderProducts(product);
-//     });
-// };
-// loadProducts();
 
 
 
 
 
+$(document).ready(function() {
+
+
+const createProduct = function(data){
+
+const $results = $(`<article class="Products" id="Products">
+<header>
+
+
+<div class="Product_image"><img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGHaecyUN3NcM7j2NKfIQiGaDXLxbuXkeMWvP8P3nQ5HadK2HL">
+
+</div>
+
+</header>
+<section class="Product_info">
+<div class="Product_name"><b>${data.name}</b></div>
+<div class="description">${data.description}</div>
+<div class="status">Status: <b>Available</b></div>
+<div class="cart_link">
+<a href="orders"><i class="fa-solid fa-heart"></i></a>
+<div class="add_Cart">
+<button type="submit" class="cart-button">Add To Cart</button>
+</div>
+</section>
+<footer>
+</div>
+</footer>
+</header>
+</article>`);
+
+return $results;
+
+
+};
+
+
+const renderProducts =(data) => {
+$(".product-container").empty();
+//loop throuh the array
+for(const index of data){
+  const $products = createProduct(index)
+  $(".product-container").prepend($products);
+}
+};
+
+const loadProducts = () => {
+  $.ajax('/products', { method: 'GET' })
+    .then((product) => {
+      renderProducts(product);
+    });
+};
+loadProducts();
 
 
 
 
 
 
-// });
+
+
+
+
+
+});
